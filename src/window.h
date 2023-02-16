d325510e66d4e4c5e05fd3eca95e77250db6a173
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2023-02-16 10:16:17 +0200

    wayland: Implement xx-pip-v1
    The xx-pip-v1 protocol provides clients a way to create floating windows
    with miniature contents, for example a video (a movie or a video call),
    a map with directions, a timer countdown, etc.
    
    These windows are placed in the overlay layer above all windows, including
    fullscreen windows.
    
    This is an experimental version of the protocol.

--- src/window.h
+++ src/window.h
@@ -765,6 +765,7 @@ public:
     virtual bool isClient() const;
     bool isDeleted() const;
     virtual bool isUnmanaged() const;
+    virtual bool isPictureInPicture() const;
 
     bool isLockScreenOverlay() const;
     void setLockScreenOverlay(bool allowed);
@@ -2054,6 +2055,11 @@ inline bool Window::isOutline() const
 }
 
 inline bool Window::isInternal() const
+{
+    return false;
+}
+
+inline bool Window::isPictureInPicture() const
 {
     return false;
 }
